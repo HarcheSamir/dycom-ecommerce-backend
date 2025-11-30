b106dda4221ec68cbef0e22b1fdce60924da77a1
HarcheSamir <harchesamir007@gmail.com>
2025-11-30 19:30:20 +0100

    a

--- src/api/admin/admin.routes.ts
+++ src/api/admin/admin.routes.ts
@@ -5,8 +5,7 @@ import {
     getAdminDashboardStats, createCourse, getAdminCourses, getCourseDetails,
     createSection, addVideoToSection, updateCourse, deleteCourse,
     updateSection, deleteSection, updateVideo, deleteVideo, updateVideoOrder,
-    getSettings, updateSettings,getMembershipPrices, updateMembershipPrices ,    updateSectionOrder , getAdminUserDetails, 
-    grantLifetimeAccess
+    getSettings, updateSettings,getMembershipPrices, updateMembershipPrices ,    updateSectionOrder 
 
 } from './admin.controller';
 // ==================== THIS IS THE FIX: Re-import the controller ====================
